03281cd1588c71ff568381e12187383fc635d1c3
Nahuel <mezzanine510@gmail.com>
2018-02-14 15:26:27 -0800

    Push project to existing repo

---
+++ README.md
@@ -0,0 +1,3 @@
+# Webpack with React
+
+Practice with adding and using webpack features to an existing React app.
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+var webpack = require('webpack');
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const VENDOR_LIBS = [
+  'react', 'lodash', 'redux', 'react-redux', 'react-dom', 'react-input-range', 'redux-form', 'redux-thunk'
+];
+
+module.exports = {
+  entry: {
+    bundle: './src/index.js',
+    vendor: VENDOR_LIBS
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    // [chunkhash] creates a new hash every time a file is altered
+    filename: '[name].[chunkhash].js'
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js?$/,
+        exclude: /node_modules/
+      },
+      {
+        use: [ 
+          'style-loader',
+          'css-loader' 
+        ],
+        test: /\.css$/
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      // [manifest] option is used for tracking changes to cached files
+      names: ['vendor', 'manifest'] 
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ]
+};
